b48a04c569b02f24fcba71edde75170124391865
Rodrigo Pereira <rp6378546@gmail.com>
2025-11-19 15:33:43 +0000

    Clean up comments in configuration_loader.py
    Removed unnecessary comments in configuration_loader.py.

--- coin_converter/config/configuration_loader.py
+++ coin_converter/config/configuration_loader.py
@@ -3,10 +3,9 @@ import yaml
 from pathlib import Path
 
 def load_config():
-    # Sobe para o diretório raiz do projeto (onde está app.py)
+    
     root = Path(__file__).resolve().parent.parent
 
-    # Caminho completo do config.yaml
     absolute_root = root / "config" / "configuration.yaml"
 
     with open(absolute_root, "r", encoding="utf-8") as f:
